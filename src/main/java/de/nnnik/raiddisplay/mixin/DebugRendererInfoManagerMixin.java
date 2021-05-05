3c0c2a22cfcf0d1595f479beb8376054f78b0354
nnnik <nnnik@web.de>
2021-05-05 21:14:20 +0200

    Fixed some formatting

--- src/main/java/de/nnnik/raiddisplay/mixin/DebugRendererInfoManagerMixin.java
+++ src/main/java/de/nnnik/raiddisplay/mixin/DebugRendererInfoManagerMixin.java
@@ -1,34 +1,35 @@
 package de.nnnik.raiddisplay.mixin;
 
 import io.netty.buffer.Unpooled;
-import java.util.Collection;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.Packet;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.packet.s2c.play.CustomPayloadS2CPacket;
+import net.minecraft.server.network.DebugInfoSender;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.village.raid.Raid;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import net.minecraft.server.network.DebugInfoSender;
-import net.minecraft.network.packet.s2c.play.CustomPayloadS2CPacket;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.network.Packet;
-import net.minecraft.network.PacketByteBuf;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.village.raid.Raid;
+
+import java.util.Collection;
 
 @Mixin(DebugInfoSender.class)
 public abstract class DebugRendererInfoManagerMixin {
-	
-	@Inject(at = @At("HEAD"), method = "sendRaids")
-	private static void sendRaids(ServerWorld world, Collection<Raid> raids, CallbackInfo ci) {
-		PacketByteBuf packetByteBuf = new PacketByteBuf(Unpooled.buffer());
-		packetByteBuf.writeInt(raids.size());
-		for (Raid r : raids) {
-			packetByteBuf.writeBlockPos(r.getCenter());
-		}
-		
-		Packet<?> packet = new CustomPayloadS2CPacket(CustomPayloadS2CPacket.DEBUG_RAIDS, packetByteBuf);
-		for (PlayerEntity player : world.toServerWorld().getPlayers()) {
-			((ServerPlayerEntity)player).networkHandler.sendPacket(packet);
-		}
-	}
+
+    @Inject(at = @At("HEAD"), method = "sendRaids")
+    private static void sendRaids(final ServerWorld world, final Collection<Raid> raids, final CallbackInfo ci) {
+        final PacketByteBuf packetByteBuf = new PacketByteBuf(Unpooled.buffer());
+        packetByteBuf.writeInt(raids.size());
+        for (final Raid r : raids) {
+            packetByteBuf.writeBlockPos(r.getCenter());
+        }
+
+        final Packet<?> packet = new CustomPayloadS2CPacket(CustomPayloadS2CPacket.DEBUG_RAIDS, packetByteBuf);
+        for (final PlayerEntity player : world.toServerWorld().getPlayers()) {
+            ((ServerPlayerEntity) player).networkHandler.sendPacket(packet);
+        }
+    }
 }
